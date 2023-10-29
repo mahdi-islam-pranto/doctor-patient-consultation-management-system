4a021bc57ee037dc18d58ad5022be445a8febef4
Mahdi Islam Pranto <74008896+mahdi-islam-pranto@users.noreply.github.com>
2023-10-29 18:52:19 +0600

    Update home.blade.php
    test github commit

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -3,5 +3,8 @@
         <center>
            This is User/Patient Dashboard
         </center>
+         <center>
+           This is User/Patient Dashboard 2
+        </center>
     </div>
 </x-app-layout>
