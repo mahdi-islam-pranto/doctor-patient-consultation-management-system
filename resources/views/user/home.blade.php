5e4ad90f7e83e792f689f00452686db4c819e9b4
Mahdi-Pranto <mahdiprantoblog@gmail.com>
2023-11-16 20:31:03 +0600

    working on user ui

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -3,5 +3,11 @@
         <center>
            This is User/Patient Dashboard
         </center>
+
+        <div>
+            <center>
+                Woking on it. 
+            </center>
+        </div>
     </div>
 </x-app-layout>
